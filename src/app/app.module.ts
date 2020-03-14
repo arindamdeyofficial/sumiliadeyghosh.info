425310bc4a9b9c421fabb10b395267ca5fc13109
Arindam Dey <biplabhome@gmail.com>
2020-03-15 01:27:22 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { CareerobjectiveComponent } from './careerobjective/careerobjective.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { LanguageComponent } from './language/language.component';
+import { HobbyComponent } from './hobby/hobby.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EducationComponent,
+    CareerobjectiveComponent,
+    ExperienceComponent,
+    CertificatesComponent,
+    LanguageComponent,
+    HobbyComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
